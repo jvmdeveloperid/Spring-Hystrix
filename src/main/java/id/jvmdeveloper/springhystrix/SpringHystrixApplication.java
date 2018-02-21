38d7c5030f121146f3ab006b5912e66590ee689b
Eko Kurniawan Khannedy <eko.k.khannedy@gdn-commerce.com>
2018-02-21 09:55:10 +0700

    create project

---
+++ src/main/java/id/jvmdeveloper/springhystrix/SpringHystrixApplication.java
@@ -0,0 +1,12 @@
+package id.jvmdeveloper.springhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHystrixApplication.class, args);
+	}
+}
